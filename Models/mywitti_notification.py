e9fae572b275cf74b7aa501ab4ab7c4c26abddd1
Jean Akpa <122117356+jeanakpa@users.noreply.github.com>
2025-07-08 00:31:07 +0000

    Add files via upload

--- Models/mywitti_notification.py
+++ Models/mywitti_notification.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 class MyWittiNotification(db.Model):
     __tablename__ = 'mywitti_notification'
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)    
     user_id = db.Column(db.Integer, db.ForeignKey('mywitti_users.id'), nullable=False)
     message = db.Column(db.String, nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
